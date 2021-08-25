5c2dabe6ad10551c3f6a9589a3cc26e75d7ccaf2
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-25 18:58:51 +0600

    finally full save department component works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { SaveDepartmentComponent } from './saveDepartment/saveDepartment.component';
 import { ViewDepartmentComponent } from './view-department/view-department.component';
 
 const routes: Routes = [
-
-  { path: '', component: AppComponent,pathMatch:'full' },
   { path: 'Create-Department', component: SaveDepartmentComponent },
-  { path: 'View-Department', component: ViewDepartmentComponent }
+  { path: 'View-Department', component: ViewDepartmentComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
