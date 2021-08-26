1f895960e11fedd90f203cb415499cf319eda3f4
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-27 01:11:35 +0600

    added on change method for constructing course url

--- src/app/view-course-stat/view-course-stat.component.ts
+++ src/app/view-course-stat/view-course-stat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, OnDestroy } from '@angular/core';
 import { NgForm, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/internal/Observable';
 
@@ -13,26 +13,30 @@ import { ViewCourseService } from '../services/view-course.service';
   templateUrl: './view-course-stat.component.html',
   styleUrls: ['./view-course-stat.component.css'],
 })
-export class ViewCourseStatComponent implements OnInit {
+export class ViewCourseStatComponent implements OnInit, OnDestroy{
+  selectedItem: any;
+  departmentList: any;
+  courseList: any;
+  departmentId = new FormControl();
+
   constructor(
     private http: HttpClient,
     private viewCourseService: ViewCourseService,
     private formBuilder: FormBuilder
   ) {}
 
-  departmentList: any;
-  courseList: any;
-  departmentId = new FormControl();
+
 
   ngOnInit() {
     this.getDepartment();
-    this.departmentId.valueChanges.subscribe(x => {
+    // this.departmentId.valueChanges.subscribe(x => {
 
-      this.viewCourseService.getCourse(x).subscribe((data: any) => {
-        this.courseList = data.data;
-        console.log(data.data);
-      });
-    })
+    //   this.viewCourseService.getCourse(x).subscribe((data: any) => {
+    //     this.courseList = data.data;
+    //     // console.log(data.data);
+    //   });
+    // })
+
   }
 
   getDepartment() {
@@ -42,5 +46,19 @@ export class ViewCourseStatComponent implements OnInit {
     });
   }
 
+  print() {
+    console.log(this.selectedItem);
+    this.viewCourseService.getCourse(this.selectedItem).subscribe(x => {
+      this.courseList = x.data;
+    })
+  }
+  ngOnDestroy() {
+
+  }
+
+  getId(x: any) {
+// console.log(this.);
+    console.log(x);
+  }
 
 }
