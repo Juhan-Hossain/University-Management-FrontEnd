0607d342776c871616957808919d6ca650068ca0
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-27 15:35:33 +0600

    added functionality of student enroll service

--- src/app/services/course-enroll.service.ts
+++ src/app/services/course-enroll.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { serviceResponse } from '../Models/serviceResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseEnrollService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  studentUrl: string = 'https://localhost:44322/api/Students/GetStudents';
+  studentCourseUrl:string ="https://localhost:44322/api/Courses/CoursesByStudentRegNo"
+
+  getStudent(): Observable<serviceResponse> {
+    return this.http.get<serviceResponse>(this.studentUrl);
+  }
+
+  getCourses(data: any) {
+    return this.http.post(`${this.studentCourseUrl}/CreateStudent`, data);
+  }
+
 }
