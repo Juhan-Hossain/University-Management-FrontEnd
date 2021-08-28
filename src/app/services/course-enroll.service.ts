bb9d26969eac52cae413a83cdf59819cf3286416
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-28 12:55:53 +0600

    added course enroll service

--- src/app/services/course-enroll.service.ts
+++ src/app/services/course-enroll.service.ts
@@ -10,14 +10,25 @@ export class CourseEnrollService {
 
   constructor(private http: HttpClient) { }
   studentUrl: string = 'https://localhost:44322/api/Students/GetStudents';
-  studentCourseUrl:string ="https://localhost:44322/api/Courses/CoursesByStudentRegNo"
+  studentCourseUrl: string = "https://localhost:44322/api/Courses/CoursesByStudentRegNo"
+  departmentUrl: string = 'https://localhost:44322/api/Departments';
+  courseEnrollUrl: string = 'https://localhost:44322/api/CourseEnroll/CreateCourseEnroll';
 
   getStudent(): Observable<serviceResponse> {
     return this.http.get<serviceResponse>(this.studentUrl);
   }
 
-  getCourses(data: any) {
-    return this.http.post(`${this.studentCourseUrl}/CreateStudent`, data);
+  getCourse(query:string) : Observable<serviceResponse> {
+
+    return this.http.get<serviceResponse>(this.studentCourseUrl + `?stdRegNo=${query}`);
+
+  }
+  getDepartment(): Observable<serviceResponse> {
+    return this.http.get<serviceResponse>(this.departmentUrl);
+  }
+  addCourseEnroll(data:any)
+  {
+    return this.http.post(this.courseEnrollUrl,data);
   }
 
 }
