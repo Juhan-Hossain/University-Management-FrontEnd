858d026d2b4e7941f23902f9ec712d44efbe84e9
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-29 19:38:51 +0600

    fixing all bug of save result component

--- src/app/services/save-result.service.ts
+++ src/app/services/save-result.service.ts
@@ -12,17 +12,30 @@ export class SaveResultService {
   departmentUrl: string = 'https://localhost:44322/api/Departments';
   courseEnrollUrl: string =
     'https://localhost:44322/api/CourseEnroll/CreateCourseEnroll';
+  studentCourseUrl: string = 'https://localhost:44322/api/Courses/EnrolledCoursesByStudentRegNo';
+
+  saveResultUrl: string = 'https://localhost:44322/api/StudentResult/CreateStudentResult';
+  gradeUrl: string = 'https://localhost:44322/api/Grades/GetGrades';
 
   getStudent(): Observable<serviceResponse> {
     return this.http.get<serviceResponse>(this.studentUrl);
   }
 
-  // getCourse(query:string) : Observable<serviceResponse> {
+  getCourse(query:string) : Observable<serviceResponse> {
 
-  //   return this.http.get<serviceResponse>(this.studentCourseUrl + `?stdRegNo=${query}`);
+    return this.http.get<serviceResponse>(this.studentCourseUrl + `?stdRegNo=${query}`);
 
-  // }
+  }
   getDepartment(): Observable<serviceResponse> {
     return this.http.get<serviceResponse>(this.departmentUrl);
   }
+  getGrade(): Observable<serviceResponse> {
+    return this.http.get<serviceResponse>(this.gradeUrl);
+  }
+
+  addStudentResult(data:any)
+  {
+    return this.http.post(this.saveResultUrl,data);
+  }
+
 }
