411d67868eac3bf6bc43a5cb61d97e17a2b3baa2
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-29 16:42:07 +0600

    added get department,student for save-result service

--- src/app/services/save-result.service.ts
+++ src/app/services/save-result.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { serviceResponse } from '../Models/serviceResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaveResultService {
+  constructor(private http: HttpClient) {}
+  studentUrl: string = 'https://localhost:44322/api/Students/GetStudents';
+  departmentUrl: string = 'https://localhost:44322/api/Departments';
+  courseEnrollUrl: string =
+    'https://localhost:44322/api/CourseEnroll/CreateCourseEnroll';
 
-  constructor() { }
+  getStudent(): Observable<serviceResponse> {
+    return this.http.get<serviceResponse>(this.studentUrl);
+  }
+
+  // getCourse(query:string) : Observable<serviceResponse> {
+
+  //   return this.http.get<serviceResponse>(this.studentCourseUrl + `?stdRegNo=${query}`);
+
+  // }
+  getDepartment(): Observable<serviceResponse> {
+    return this.http.get<serviceResponse>(this.departmentUrl);
+  }
 }
