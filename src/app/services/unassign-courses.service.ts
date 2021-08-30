cd3e8695b8b83b7b0b3f3334735dd442f06c64e1
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-31 02:01:25 +0600

    added unassign all courses service

--- src/app/services/unassign-courses.service.ts
+++ src/app/services/unassign-courses.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { serviceResponse } from '../Models/serviceResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnassignCoursesService {
+  constructor(private http: HttpClient) {}
+  Unassignurl: string =
+    'https://localhost:44322/api/DeletedCourseAssign/UnAssaignAllCourses';
 
-  constructor() { }
+  UnassignAllCourses(): Observable<serviceResponse> {
+    return this.http.delete<serviceResponse>(this.Unassignurl);
+  }
 }
