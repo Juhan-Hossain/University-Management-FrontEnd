07d325d74be66e6871e2e260c01d8741240b9211
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-31 13:33:04 +0600

    added course-assign functionality

--- src/app/course-assign-to-teacher/course-assign-to-teacher.component.ts
+++ src/app/course-assign-to-teacher/course-assign-to-teacher.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { NgForm, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { course } from '../Models/course';
+import { student } from '../Models/student';
+import { department } from '../Models/department';
+import { CourseAssignService } from '../services/course-assign.service';
+
+@Injectable({
+  providedIn: 'root',
+})
 
 @Component({
   selector: 'app-course-assign-to-teacher',
@@ -7,9 +21,148 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseAssignTOTeacherComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private courseAssign: CourseAssignService,
+    private formBuilder: FormBuilder
+  ) { }
 
+  studentList: student[]=[];
+  courseList: course[] = [];
+  depatmentList: department[] = [];
+  teacherList: any;
+  selectedDepartment: any;
+  selectedCourse: course[] = [];
+  selectedTeacher: any;
+  selectedCode: any;
+
+  myForm = this.formBuilder.group({
+    courseName: new FormControl(''),
+    department: new FormControl('',Validators.required),
+    teacher: new FormControl('',Validators.required),
+    creditToBeTaken: new FormControl(''),
+    remainingCredit:new FormControl(''),
+    courseCode: new FormControl('', Validators.required),
+    courseCredit:new FormControl('')
+  });
   ngOnInit(): void {
+    this.getDepartments();
+  }
+
+  get myFormControl() {
+    return this.myForm.controls;
+  }
+
+  getDepartments() {
+    this.courseAssign.getDepartment().subscribe((data: any) => {
+      this.depatmentList = data.data;
+      // this.selectedDepartment = this.myForm.controls
+      //   .department.value;
+
+    });
+  }
+
+  onSubmit() {
+    // this.myForm.controls.courseName.setValue('');
+    // this.myForm.controls.creditToBeTaken.setValue('');
+    // this.myForm.controls.remainingCredit.setValue('');
+    // this.myForm.controls.courseCredit.setValue('');
+
+    console.log(this.myForm.value);
+    this.courseAssign.addCourseAssign(this.myForm.value).subscribe(
+      (obj: any) => {
+        console.log(obj.data);
+
+        alert(obj.message);
+        // this.myForm.controls.studentRegNo.setValue('');
+        // this.myForm.controls.courseName.setValue('');
+        // this.myForm.controls.gradeLetter.setValue('');
+      },
+      (er: any) => {
+        alert(er.error.message);
+      }
+    );
+
+  }
+  changeDeptId(x: any) {
+    this.courseAssign.getTeacher(x).subscribe(
+      (obj1) => {
+        // this.getDepartments();
+        this.teacherList = obj1.data;
+        // console.log(this.teacherList);
+
+
+        // console.log(this.myForm.value);
+
+
+      },
+      (er1: any) => {
+        console.log(er1);
+        // alert(er1.error.message);
+      }
+    );
+
+    //---------------------------------
+    this.courseAssign.getCourse(x).subscribe(
+      (obj1) => {
+        // this.getDepartments();
+        this.courseList = obj1.data;
+        // console.log(this.courseList);
+        // console.log(this.myForm.value);
+
+      },
+      (erMain: any) => {
+        console.log(erMain);
+        alert(erMain.error.message);
+      }
+    );
+  }
+
+
+
+  changeTeacher(y:any)
+  {
+    this.getDepartments();
+    this.selectedTeacher = y;
+    console.log(y);
+    console.log(this.teacherList);
+    this.selectedTeacher= this.myForm.controls
+    .teacher.value;
+    let selectedCreditToTaken = this.teacherList.find(
+      (px: any) =>
+        px.name == y
+    ).creditToBeTaken;
+    let selectedRemainingCredit = this.teacherList.find(
+      (px: any) =>
+        px.name == y
+    ).remainingCredit;
+
+    console.log(selectedCreditToTaken);
+    this.myForm.controls.courseName.setValue(selectedCreditToTaken);
+
+    this.myForm.controls.remainingCredit.setValue(selectedRemainingCredit);
+    console.log(this.myForm.controls.remainingCredit.value);
+      console.log(this.myForm.controls.creditToBeTaken.value);
+
+
+  }
+  changeCourseControl(x:any) {
+    this.selectedCode = x;
+    console.log(x);
+    console.log(this.courseList);
+    // this.getDepartments();
+    let selectedcourseName = this.courseList.find(
+      (px: any) =>
+        px.code === this.selectedCode
+    )?.name;
+    let selectedCourseCredit = this.courseList.find(
+      (x: any) =>
+        x.code === this.selectedCode
+    )?.credit;
+
+    this.myForm.controls.courseName.setValue(selectedcourseName);
+
+    this.myForm.controls.courseCredit.setValue(selectedCourseCredit);
   }
 
 }
--- src/app/services/course-assign.service.ts
+++ src/app/services/course-assign.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { serviceResponse } from '../Models/serviceResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseAssignService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  departmentUrl: string = 'https://localhost:44322/api/Departments';
+  courseurl: string =
+    'https://localhost:44322/api/Courses/ViewCoursesByDepartment';
+    teacherurl: string =
+      'https://localhost:44322/api/Teachers/Department/';
+  courseAssign: string = 'https://localhost:44322/api/CourseAssignment/CreateCourseAssignment';
+
+    getDepartment(): Observable<serviceResponse> {
+      return this.http.get<serviceResponse>(this.departmentUrl);
+    }
+    getTeacher(query:number) : Observable<serviceResponse> {
+
+      return this.http.get<serviceResponse>(this.teacherurl+query);
+    }
+
+    getCourse(query:number) : Observable<serviceResponse> {
+
+      return this.http.get<serviceResponse>(this.courseurl+`?departmentId=${query}`);
+    }
+  addCourseAssign(data:any) {
+    return this.http.post(this.courseAssign,data);
+  }
 }
